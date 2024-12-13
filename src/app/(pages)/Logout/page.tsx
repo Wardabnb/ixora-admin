29698f690707a173da8eccf536b4838b502dfbd8
Warda Bounab <wardabounab@MacBook-Pro-de-Warda.local>
2024-12-13 19:29:01 +0100

    fit(admin)

--- src/app/(pages)/Logout/page.tsx
+++ src/app/(pages)/Logout/page.tsx
@@ -1,16 +1,14 @@
 "use client";
 
-import { useEffect } from "react";
 import { redirect } from "next/navigation";
 
 const Page = () => {
-  useEffect(() => {
-    // Ensure this code only runs on the client side
-    if (typeof window !== "undefined") {
-      localStorage.removeItem("admin");
-      redirect("/login");
-    }
-  }, []); // The effect only runs once after the initial render
+  // Ensure this code only runs on the client side
+
+  localStorage.removeItem("admin");
+  redirect("/login");
+
+  // The effect only runs once after the initial render
 
   return <div></div>;
 };
