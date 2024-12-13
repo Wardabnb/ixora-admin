732f57449fb61c08d4004fed37a0a2c82372920c
Warda Bounab <wardabounab@MacBook-Pro-de-Warda.local>
2024-12-13 16:33:53 +0100

    feat(admin)

--- src/app/(pages)/Reservation_Flight/page.tsx
+++ src/app/(pages)/Reservation_Flight/page.tsx
@@ -1,10 +1,21 @@
 import { TabsDemo } from "@/components/tabs";
 import { TabsFlight } from "@/components/tabsFlight";
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {};
 
 const page = (props: Props) => {
+  const [flightsData, setFlightsData] = useState<string | null>(null);
+
+  // Assurez-vous de vérifier si la valeur est `null`
+  const savedFlights = localStorage.getItem("reservationFlight");
+
+  if (savedFlights !== null) {
+    setFlightsData(savedFlights); // Vous pouvez maintenant assigner la string
+  } else {
+    setFlightsData(null); // Sinon, définissez-le sur null
+  }
+
   return (
     <div className="mt-5">
       <TabsFlight />
