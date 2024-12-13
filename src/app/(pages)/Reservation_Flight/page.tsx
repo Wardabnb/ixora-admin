8fb9c84f00ea90406353774d4ec94e97a44b0398
Warda Bounab <wardabounab@MacBook-Pro-de-Warda.local>
2024-12-13 19:25:23 +0100

    fit(admin)

--- src/app/(pages)/Reservation_Flight/page.tsx
+++ src/app/(pages)/Reservation_Flight/page.tsx
@@ -6,19 +6,6 @@ import { TabsFlight } from "@/components/tabsFlight";
 type Props = {};
 
 const Page = (props: Props) => {
-  const [flightsData, setFlightsData] = useState<string | null>(null);
-
-  useEffect(() => {
-    // Vérification côté client pour accéder à localStorage
-    const savedFlights = localStorage.getItem("reservationFlight");
-
-    if (savedFlights !== null) {
-      setFlightsData(savedFlights); // Assignez la valeur récupérée de localStorage
-    } else {
-      setFlightsData(null); // Si rien n'est trouvé, définissez à null
-    }
-  }, []); // Le tableau vide [] garantit l'exécution seulement après le premier rendu
-
   return (
     <div className="mt-5">
       <TabsFlight />
